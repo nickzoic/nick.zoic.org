f940e4d3440ccef08102a651e32ccfa39757c660
Nick Moore <nick@zoic.org>
2021-05-03 14:09:14 +1000

    ...........

--- art/writing-an-apple-2-game-in-2021-3/json/disks/index.js
+++ art/writing-an-apple-2-game-in-2021-3/json/disks/index.js
@@ -3,5 +3,10 @@ disk_index = [
     "filename": "json/disks/audit.json",
     "name": "Apple II Audit",
     "category": "Utility"
+  },
+  { 
+    "filename": "json/disks/hello.json", 
+	  "name": "hello",
+	  "category": "unknown"
   }
 ];

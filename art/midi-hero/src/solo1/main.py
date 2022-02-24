157d8c1d1672bfe9bc771f53c4f39887f73308fb
Nick Moore <nick@zoic.org>
2022-02-24 19:21:07 +1100

    ha ha!

--- art/midi-hero/src/solo1/main.py
+++ art/midi-hero/src/solo1/main.py
@@ -1,54 +1,72 @@
 import usb_midi
 import digitalio
+import analogio
 import board
 
 def make_button(pin):
     btn = digitalio.DigitalInOut(pin)
     btn.direction = digitalio.Direction.INPUT
-    btn.pull = digitalio.Pull.UP
+    btn.pull = digitalio.Pull.DOWN
     return btn
 
-s_up = make_button(board.D6)
-s_dn = make_button(board.D7)
+fret_green = make_button(board.D2)
+fret_red = make_button(board.D3)
+fret_yellow = make_button(board.D4)
+fret_blue = make_button(board.D5)
+fret_orange = make_button(board.D6)
 
-f_gn = make_button(board.D8)
-f_rd = make_button(board.D9)
-f_yy = make_button(board.D10)
-f_bu = make_button(board.D11)
-f_or = make_button(board.D12)
+button_select = make_button(board.D8)
+button_start = make_button(board.D9)
 
-chords = {
-        (True, False, False, False, False): [ 60 ],    # C
-        (True, False, True, False, False): [ 61 ],     # C♯
-        (True, True, False, False, False): [ 62 ],     # D
-        (True, True, True, False, False): [ 63 ],      # D♯
-        (False, True, False, False, False): [ 64 ],    # E
-        (False, True, True, False, False): [ 65 ],     # F
-        (False, True, False, True, False): [ 66 ],     # F♯
-        (False, False, True, False, False): [ 67 ],    # G
-        (False, False, True, False, True): [ 68 ],     # A♯
-        (False, False, True, True, False): [ 69 ],     # A
-        (False, False, False, True, True): [ 70 ],     # B♭
-        (False, False, False, True, False): [ 71 ],    # B
-        (False, False, False, False, True): [ 72 ],    # C
+strum_down = make_button(board.D10)
+strum_up = make_button(board.D11)
+
+pitch_wheel = analogio.AnalogIn(board.A0)
+
+notes = {
+        (True, False, False, False, False): 60,    # C
+        (True, False, True, False, False):  61,     # C♯
+        (True, True, False, False, False):  62,     # D
+        (True, True, True, False, False):   63,      # D♯
+        (False, True, False, False, False): 64,    # E
+        (False, True, True, False, False):  65,     # F
+        (False, True, False, True, False):  66,     # F♯
+        (False, False, True, False, False): 67,    # G
+        (False, False, True, False, True):  68,     # A♯
+        (False, False, True, True, False):  69,     # A
+        (False, False, False, True, True):  70,     # B♭
+        (False, False, False, True, False): 71,    # B
+        (False, False, False, False, True): 72,    # C
     }
 
 midi_out = usb_midi.ports[1]
 
+# All notes off
+midi_out.write(bytes((0xb0, 123, 0)))
+
 while True:
 
-    while s_up.value and s_dn.value: pass
+    while not strum_up.value and not strum_down.value: pass
 
-    notes = chords.get((not f_gn.value, not f_rd.value, not f_yy.value, not f_bu.value, not f_or.value), [])
+    frets = (fret_green.value, fret_red.value, fret_yellow.value, fret_blue.value, fret_orange.value)
+    note = notes.get(frets, None)
+    
+    if note is not None:
+        if strum_up.value: note = note + 12
+        print(note)
 
-    # play single notes on down, fifths on up
-    if not s_up.value: notes = [ notes[0], notes[0] + 7 ]
+        midi_out.write(bytes((0x90, note, 0x3f)))
 
-    for note in notes:
-        midi_out.write(bytes((0x90, note, 0x5f)))
-
-    while not s_up.value or not s_dn.value: pass
-
-    for note in notes:
+        while strum_up.value or strum_down.value:
+            
+            pitch_down = (65000 - pitch_wheel.value) >> 3
+            if pitch_down < 0: pitch_down = 0
+            
+            pitch_down = 0x2000 - pitch_down
+            print(pitch_down)
+            pitch_l = pitch_down & 0x7F
+            pitch_h = pitch_down >> 7
+            midi_out.write(bytes((0xE0, pitch_l, pitch_h)))
+            
         midi_out.write(bytes((0x80, note, 0)))
 

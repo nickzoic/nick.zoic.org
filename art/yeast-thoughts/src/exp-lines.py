cfc9317f29db4cf1179e280cface53f6b11684bf
Nick Moore <nick@zoic.org>
2025-09-24 11:56:20 +1000

    exponential graphs

---
+++ art/yeast-thoughts/src/exp-lines.py
@@ -0,0 +1,28 @@
+from matplotlib import pyplot
+import numpy as np
+
+scores = [0, 0.5, 0.75, 0.9, 1]
+
+def pop(a, t):
+    return (1+a)**t
+
+times = np.arange(0, 30, 0.1)
+
+lines = [list() for _ in scores]
+
+for t in times:
+    pops = [pop(s, t) for s in scores]
+    total = sum(pops)
+
+    for n, p in enumerate(pops):
+        lines[n].append(p/total)
+
+pyplot.xlabel("time")
+pyplot.ylabel("fraction")
+
+for score, line in zip(scores, lines):
+    pyplot.plot(times, line, label="score %.2f" % score)
+
+pyplot.legend(loc="upper left", title="variants", reverse=True)
+pyplot.savefig("exp-lines.svg", bbox_inches="tight")
+#pyplot.show()
---
+++ art/yeast-thoughts/src/exp-stack.py
@@ -0,0 +1,28 @@
+from matplotlib import pyplot
+import numpy as np
+
+scores = [1, 0.9, 0.75, 0.5, 0]
+
+scores = [0, 0.5, 0.75, 0.9, 1]
+def pop(a, t):
+    return (1+a)**t
+
+times = np.arange(0, 30, 0.1)
+
+lines = [list() for _ in scores]
+
+for t in times:
+    pops = [pop(s, t) for s in scores]
+    total = sum(pops)
+
+    for n, p in enumerate(pops):
+        lines[n].append(p/total)
+
+pyplot.xlabel("time")
+pyplot.ylabel("fraction")
+
+pyplot.stackplot(times, *lines, labels=["score %.2f" % s for s in scores])
+
+pyplot.legend(loc="upper right", title="variants", reverse=True)
+pyplot.savefig("exp-stack.svg", bbox_inches="tight")
+#pyplot.show()
